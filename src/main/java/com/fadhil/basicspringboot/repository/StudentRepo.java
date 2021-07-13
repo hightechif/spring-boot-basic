982009bef11e37439e1f93c2cc7ab01f1d3b2560
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-13 22:12:56 +0700

    student repository

--- src/main/java/com/fadhil/basicspringboot/repository/StudentRepo.java
+++ src/main/java/com/fadhil/basicspringboot/repository/StudentRepo.java
@@ -4,7 +4,14 @@ import com.fadhil.basicspringboot.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
+
+    // SELECT * FROM student WHERE email = ?
+    // @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+    Iterable<Student> getByEmail(String email);
 
 }
